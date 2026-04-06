d7c30c6a46e66558b1797175dc30b3ffca98b6ac
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-06 13:38:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-python-pytest-setup/refs/heads/main/mise.toml" -o "mise.toml"
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-python-pytest-setup/refs/heads/main/requirements.txt" -o "requirements.txt"
-mise install
-mise use
-python -m venv .venv
+mise exec python@3 -- python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
