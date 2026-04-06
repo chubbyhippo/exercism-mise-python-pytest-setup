b8c8c6e76f4bfc220412673225725035d1e2da27
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-06 13:31:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-python-pytest-setup/refs/heads/main/mise.toml" -o "mise.toml"
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-python-pytest-setup/refs/heads/main/requirements.txt" -o "requirements.txt"
+mise install
